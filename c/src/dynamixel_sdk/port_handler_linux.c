784d8c7dae6281f7bec273c8917022b015410f5c
yewon24 <ywon526@gmail.com>
2020-08-28 17:49:18 +0900

    wip

--- c/src/dynamixel_sdk/port_handler_linux.c
+++ c/src/dynamixel_sdk/port_handler_linux.c
@@ -280,12 +280,12 @@ uint8_t setupPortLinux(int port_num, int cflag_baud)
   tcflush(portData[port_num].socket_fd, TCIFLUSH);
   tcsetattr(portData[port_num].socket_fd, TCSANOW, &newtio);
 
-  // ioctl (portData[port_num].socket_fd, TIOCMGET, &status);
+  ioctl (portData[port_num].socket_fd, TIOCMGET, &status);
 
-  // status |= TIOCM_DTR ;
-  // status |= TIOCM_RTS ;
+  status |= TIOCM_DTR ;
+  status |= TIOCM_RTS ;
 
-  // ioctl (portData[port_num].socket_fd, TIOCMSET, &status);
+  ioctl (portData[port_num].socket_fd, TIOCMSET, &status);
 
   // usleep (10000) ;	// 10mS
   // digitalWrite(4, HIGH);
