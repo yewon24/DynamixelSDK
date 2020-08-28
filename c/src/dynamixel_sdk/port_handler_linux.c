3633a1e76157ab36d597853a4fc3353422284859
yewon24 <ywon526@gmail.com>
2020-08-28 17:22:02 +0900

    wip

--- c/src/dynamixel_sdk/port_handler_linux.c
+++ c/src/dynamixel_sdk/port_handler_linux.c
@@ -200,7 +200,7 @@ int writePortLinux(int port_num, uint8_t *packet, int length)
 
   ret=write(portData[port_num].socket_fd, packet, length);
 
-  // usleep(10000);
+  usleep(10000);
   digitalWrite(4, LOW);
 
   return ret;
